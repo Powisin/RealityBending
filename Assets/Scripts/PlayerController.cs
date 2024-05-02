cd3ae3e42de613e02ede219a26f998861a4ee3ce
Esther Escrihuela <estherescrihuela2503@gmail.com>
2024-05-02 20:21:39 +0200

    gravedad fallida

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,7 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEditor.Rendering;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class PlayerController : MonoBehaviour
 {
@@ -9,16 +11,28 @@ public class PlayerController : MonoBehaviour
 
     public float speed = 5;
 
+    [SerializeField] private float _gravity;
+    [SerializeField] private float _fallVelocity;
+
+
     public float turnSmoothTime = 0.1f;
     float turnSmoothVelocity;
 
     public Transform cam;
 
+
+    private void Awake()
+    {
+        _gravity = 60f; //gravedad normal -9.81 aprox
+    }
+
+
     void Update()
     {
+        SetGravity();
         float horizontal = Input.GetAxisRaw("Horizontal");
         float vertical = Input.GetAxisRaw("Vertical");
-
+        
 
         Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
 
@@ -31,11 +45,27 @@ public class PlayerController : MonoBehaviour
 
             Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
 
-            transform.rotation = Quaternion.Euler(0f, angle, 0f); 
+            transform.rotation = Quaternion.Euler(0f, angle, 0f);
+
+            
 
             controller.Move(moveDir.normalized * speed * Time.deltaTime);
+
+            
         }
 
+        
+    }
+    public void SetGravity() 
+    {
+        if (controller.isGrounded) 
+        {
+            _fallVelocity = -_gravity * Time.deltaTime;
+        }
+        else 
+        {
+            _fallVelocity -= _gravity * Time.deltaTime;
+        }
         
     }
 }
