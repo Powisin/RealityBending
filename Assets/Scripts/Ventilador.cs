803f2522b21e0ba18018137cc39d8a724e375499
Esther Escrihuela <estherescrihuela2503@gmail.com>
2024-05-04 21:27:41 +0200

    odio unity y el vector3

---
+++ Assets/Scripts/Ventilador.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ventilador : MonoBehaviour
+{
+
+    [SerializeField] private GameObject objectToAddForce;
+    [SerializeField] private float maxForce;
+    [SerializeField] private float minForce;
+    [SerializeField] private Vector3 forceDirectionVector;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            float forceMafnitude = Random.Range(minForce, maxForce);
+
+            Vector3 force = Vector3.Normalize(forceDirectionVector) * forceMafnitude;
+
+            objectToAddForce.GetComponent<Rigidbody>().AddForce(force);
+
+            Debug.Log("Si sale esto no entiedo pues");
+        }
+    }
+
+
+}
