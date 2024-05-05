5a261059d333bd5f06484db29a30a6720eca6637
Esther Escrihuela <estherescrihuela2503@gmail.com>
2024-05-05 11:42:53 +0200

    me voa matar

---
+++ Assets/Scripts/CogerObjeto.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CogerObjeto : MonoBehaviour
+{
+    public GameObject handPoint;
+
+    private GameObject pickedObject = null;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (pickedObject = null)
+        {
+            if (Input.GetKeyDown(KeyCode.Q)) 
+            {
+                pickedObject.GetComponent<Rigidbody>().useGravity = true;
+
+                pickedObject.GetComponent<Rigidbody>().isKinematic = false;
+
+                pickedObject.gameObject.transform.SetParent(null);
+
+                pickedObject = null;
+            }
+
+        }
+        
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.CompareTag("Objeto")) 
+        {
+            if (Input.GetKeyDown(KeyCode.E) && pickedObject == null) 
+            {
+                other.GetComponent<Rigidbody>().useGravity = false;
+
+                other.GetComponent <Rigidbody>().isKinematic = true;
+
+                other.transform.position = handPoint.transform.position;
+
+                other.gameObject.transform.SetParent(handPoint.gameObject.transform);
+
+                pickedObject = other.gameObject;
+            }
+        }
+    }
+}
