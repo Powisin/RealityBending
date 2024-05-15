e94270ededfae5c48a418c0b0ec839681faef49f
EduardBeltranLopez <edubeltranlp@gmail.com>
2024-05-15 11:32:11 +0200

    Checkpoint

--- Assets/08_Scripts/GameRespawn.cs
+++ Assets/08_Scripts/GameRespawn.cs
@@ -1,18 +1,43 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameRespawn : MonoBehaviour
 {
     public float threshold;
+    [SerializeField] GameObject[] checkPoints;
+    [SerializeField] GameObject checkPointsCanvas;
+    private GameObject checkPointCurrent;
 
 
+    private void Start()
+    {
+        checkPointCurrent = checkPoints[0];
+    }
     // Update is called once per frame
     void FixedUpdate()
     {
        if (transform.position.y < threshold)
         {
-            transform.position = new Vector3(41.6f, 2, 0f);
-        } 
+            transform.position = new Vector3(checkPointCurrent.transform.position.x, checkPointCurrent.transform.position.y, checkPointCurrent.transform.position.z);
+        }  
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("BreakingDoor0"))
+        {
+            checkPointCurrent = checkPoints[1];
+            checkPointsCanvas.SetActive(true);
+        }
+    }
+
+    public void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("BreakingDoor0"))
+        {
+            checkPointsCanvas.SetActive(false);
+        }
     }
 }
